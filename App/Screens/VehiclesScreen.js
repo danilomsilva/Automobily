11826ff94fda8fb26c9fcfc11c327ddbb879cee8
danilomsilva <>
2022-06-14 23:26:20 +0100

    installed drawer and tab navigation

---
+++ App/Screens/VehiclesScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const VehiclesScreen = () => {
+  return (
+    <View>
+      <Text>VehiclesScreen</Text>
+    </View>
+  );
+};
+
+export default VehiclesScreen;
